ffb39a3c6e233ea03647c03e2947425543f9c949
Pim Keer <pimk@ntnu.no>
2023-05-06 12:48:48 +0200

    Added find_N_lp.

--- optimal_grid.R
+++ optimal_grid.R
@@ -1,6 +1,6 @@
 source("functions.R")
 
-find_N <- function(n_row, col, N_benchmark = NULL){
+find_N <- function(n_row, col, type, N_benchmark = NULL){
   if(is.null(N_benchmark)){
     N_benchmark <- 4 * max(n_row)
   }
@@ -10,11 +10,13 @@ find_N <- function(n_row, col, N_benchmark = NULL){
 
   order_benchmark <- supremum_ordering(n_row, 
                                        col, 
+                                       type = type,
                                        N_order = N_benchmark, 
                                        N_find = N_benchmark, 
                                        pre_tables = tables,
                                        pre_group_reduced = group_reduced,
                                        show_progress = TRUE)[[1]]
+  print(order_benchmark)
   N_old <- N_benchmark
   N_new <- round(N_benchmark / 2)
   k <- 2
@@ -24,11 +26,14 @@ find_N <- function(n_row, col, N_benchmark = NULL){
     N_old <- N_new
     order_new <- supremum_ordering(n_row, 
                                    col, 
+                                   type = type,
                                    N_order = N_new, 
                                    N_find = N_new, 
                                    pre_tables = tables,
                                    pre_group_reduced = group_reduced,
                                    show_progress = FALSE)[[1]]
+    print(order_new)
+    
     if(order_diff(order_benchmark, order_new) == 0){
       N_new <- round(N_new - N_benchmark / 2 ^ k)
     }
@@ -45,11 +50,73 @@ find_N <- function(n_row, col, N_benchmark = NULL){
   }
 }
 
-n_arr <- 3:5
+find_N_lp <- function(n_row, col, alpha = 0.05, N_benchmark = 1000){
+  if(is.null(N_benchmark)){
+    N_benchmark <- 4 * max(n_row)
+  }
+  
+  tables <- gen_tables(n_row, col)
+  group_reduced <- group_reduce(tables)
+  
+  K_benchmark <- lp_solver(n_row, 
+                            col,
+                            alpha,
+                            N = N_benchmark,
+                            type = "sym",
+                            pre_tables = tables,
+                            pre_group_reduced = group_reduced,
+                            pre_A = NULL,
+                            solver = "gurobi",
+                            auxiliary = FALSE,
+                            group_length_coefficients = TRUE,
+                            scaling = TRUE,
+                            show_progress = TRUE)[[1]]
+
+  print(K_benchmark)
+  N_old <- N_benchmark
+  N_new <- round(N_benchmark / 2)
+  k <- 2
+  
+  while(abs(N_new - N_old) > 1){
+    print(N_new)
+    N_old <- N_new
+    K_new <- lp_solver(n_row, 
+                             col,
+                             alpha,
+                             N = N_new,
+                             type = "sym",
+                             pre_tables = tables,
+                             pre_group_reduced = group_reduced,
+                             pre_A = NULL,
+                             solver = "gurobi",
+                             auxiliary = FALSE,
+                             group_length_coefficients = TRUE,
+                             scaling = TRUE,
+                             show_progress = TRUE)[[1]]
+    print(K_new)
+    
+    if(order_diff(K_benchmark, K_new) == 0){
+      N_new <- round(N_new - N_benchmark / 2 ^ k)
+    }
+    else{
+      N_new <- round(N_new + N_benchmark / 2 ^ k)
+    }
+    k <- k + 1
+  }
+  if(N_new > N_old){
+    return(N_new)
+  }
+  else{
+    return(N_old)
+  }
+}
+
+k_arr <- 1:10
+n_arr <- 5 * k_arr
 N_arr <- c()
 for(n in n_arr){
   print(n)
-  N_arr <- append(N_arr, find_N(c(n,n,n), 2, 1000))
+  N_arr <- append(N_arr, find_N(c(n,n), 2, "sym", 1000))
 }
 plot(n_arr, N_arr)
 
